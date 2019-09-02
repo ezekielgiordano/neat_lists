ba9f30e81e5cab8d60e456a2e74b5327125ef0af
Ezekiel Giordano <etgfiles@Ezekiels-Air.fios-router.home>
2019-09-01 20:32:17 -0400

    Added ability to use yarn start

--- app/javascript/react/components/ArmyDeletionTile.js
+++ app/javascript/react/components/ArmyDeletionTile.js
@@ -1,45 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ArmyDeletionTile extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			id: this.props.id,
-			name: this.props.name
-		}
-		this.deleteFromDatabase = this.deleteFromDatabase.bind(this)
-	}
-
-	deleteFromDatabase() {
-		fetch(`/api/v1/armies/${this.state.id}`, {
-			method: 'DELETE',
-			credentials: 'same-origin',
-        	headers: {'Content-Type': 'application/json'}
-		})
-		.then(response => {
-			if (response.ok) {
-				return response
-			} else {
-				let errorMessage = `${response.status} (${response.statusText})`,
-				error = new Error(errorMessage)
-				throw(error)
-			}
-		})
-		.then(response => response.json())
-		.catch(error => console.error(`Error in fetch: ${error.message}`))
-		this.props.hideDeletionTile()
-		this.props.showDeletionSuccessMessage()
-	}	
-
-	render() {
-		return (
-			<div>
-				<p>Are you sure you want to delete "{this.state.name}"?</p>
-				<button onClick={this.props.hideDeletionTile}>Cancel</button>
-				<button onClick={this.deleteFromDatabase}>Delete</button>
-			</div>
-		)
-	}
+const ArmyDeletionTile = props => {
+	return (
+		<div>
+			<p>Are you sure you want to delete "{props.name}"?</p>
+			<button onClick={props.hideDeletionTile}>Cancel</button>
+			<button onClick={props.deleteFromDatabase}>Delete</button>
+		</div>
+	)
 }
 
 export default ArmyDeletionTile
